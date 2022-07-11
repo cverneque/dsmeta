000fcada65d2a1c35431148a07b7f40af6b651f6
Cristian Verneque <cristian_werneck@hotmail.com>
2022-07-11 19:13:01 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+
+function Header() {
+    return (
+        <header>
+            <div className="dsmeta-logo-conteiner">
+                <img src={logo} alt="DsMeta" />
+                <h1>DsMeta</h1>
+                <p>Desenvolvido por
+                    <a href="https://www.linkedin.com/in/cristian-camillo-verneque-66655b24/">@CVerneque</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header
--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,5 +1,6 @@
 import icon from '../../assets/img/notification-icon.svg'
-import './style.css'
+
+import './styles.css'
 
 function NotificationButton() {
     return(
